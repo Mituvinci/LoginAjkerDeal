e0d1e91f8628a031497803a582d729b8fc086bea
mitu <catvinchy@gmail.com>
2016-06-14 13:15:15 +0600

    Initial commit

---
+++ app/src/androidTest/java/friendlychat/codelab/firebase/google/com/loginajkerdeal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package friendlychat.codelab.firebase.google.com.loginajkerdeal;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
